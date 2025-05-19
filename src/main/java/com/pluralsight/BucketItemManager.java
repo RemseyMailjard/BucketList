c3e08ab2d9b671aa9032173ca20f5b513304a0df
Remsey Mailjard <remsey@skills4-it.nl>
2025-05-19 20:39:14 +0200

    Update SQL database
    1

--- src/main/java/com/pluralsight/BucketItemManager.java
+++ src/main/java/com/pluralsight/BucketItemManager.java
@@ -5,7 +5,10 @@ import java.util.List;
 public interface BucketItemManager {
     void addItem(BucketItem item);
     void removeItem(String title);
+
     void updateItem(String title);
+
+    void updateItem(String title, String newTitle);
     void markItemAsDone(String title);
 
     List<BucketItem> getAllItems();
